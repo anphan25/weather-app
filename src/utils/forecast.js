7e552fe68cc5548e84cce11a9e1d4d6b0cf0f913
anphan25 <73126703+anphan25@users.noreply.github.com>
2021-08-14 19:51:23 +0700

    Add Humidity

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -4,7 +4,7 @@ const forecast = (latitude, longitude, callback) => {
   const url = `http://api.weatherstack.com/current?access_key=6d44022c5ad92ff1d449f6e5804b0192&query=${latitude},${longitude}&units=f`;
   request({ url, json: true }, (error, { body }) => {
     if (error) {
-      callback("Unable to coonect cc", undefined);
+      callback("Unable to connect cc", undefined);
     } else if (body.error) {
       callback("Unable to find location", undefined);
     } else {
@@ -14,7 +14,7 @@ const forecast = (latitude, longitude, callback) => {
           (body.current.temperature - 32) / 1.8
         )} degrees out. It feels like ${Math.round(
           (body.current.feelslike - 32) / 1.8
-        )} degress out`
+        )} degrees out and the Humidity is ${body.current.humidity}%`
       );
     }
   });
